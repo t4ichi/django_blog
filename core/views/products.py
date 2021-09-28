0e6747cc0fc775872274983cafea790f0caf5495
t4ichi <s2k.hakurin@gmail.com>
2021-09-28 09:04:03 +0900

    商品詳細ページを作成

--- core/views/products.py
+++ core/views/products.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from core.models import ProductModel
 
@@ -11,6 +11,20 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['PAGE_TITLE'] = '商品一覧'
+
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = ProductModel
+    template_name = 'main/product_detail.html'
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['PAGE_TITLE'] = '商品詳細'
 
         return context
 
